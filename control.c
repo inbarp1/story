d3dd8afa3b95770ec58cdf4ecc77d60d43f53a1b
Inbar Pe'er <inbarpeer@Inbars-MacBook-Air.local>
2017-12-08 11:35:42 -0500

    added

--- control.c
+++ control.c
@@ -8,18 +8,15 @@
 #define KEYONE 010600
 #define KEYTWO 666;
 int main(int argc, char *argv[]){
-  int sem_id, mem_id;
+  int sem_id, mem_id, fd, attach_mem; 
  if (argc==2) {
    if (!strcmp(argv[1],"-v")) {
-     mem_id = shmget(KEYTWO,sizeof(char *), IPC_CREAT | IPC_EXCL | 0600);
-     
      sem_id=semget(KEYONE,1,0600);
-     printf("Semaphore value: %d\n",semctl(sem_id,0,GETVAL));
+     //printf("Semaphore value: %d\n",semctl(sem_id,0,GETVAL));
     }
     else if (!strcmp(argv[1],"-r")) {
       sem_id=semget(KEYONE,1,0600);
-      mem_id=
-      printf("Semaphore removed: %d\n",semctl(sem_id,0,IPC_RMID));
+	//printf("Semaphore removed: %d\n",semctl(sem_id,0,IPC_RMID));
     }
     else
       printf("Improper input\n");
@@ -29,10 +26,11 @@ int main(int argc, char *argv[]){
       
      sem_id=semget(KEYONE,1,IPC_CREAT | IPC_EXCL | 0600);
      if (sem_id>=0) {
-       int size=0;
-       sscanf(argv[2],"%d",&size);
        //printf("Semaphore created: %d\n",sem_id);
-       semctl(sem_id,0,SETVAL,size);
+       semctl(sem_id,0,SETVAL,1);
+       mem_id = shmget(KEYTWO,sizeof(char *), IPC_CREAT | IPC_EXCL | 0600);
+       fd = open("story.txt" ,O_CREAT| O_TRUNC | O_RDWR, 0600);
+       attach_mem = shmat(sem_id,0, 0);
        //printf("Value set: %d\n", semctl(sem_id,0,GETVAL));
      }
      else {
